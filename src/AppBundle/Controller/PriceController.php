5cb33100e5a88ec6b3b55894cfc1b5e54672af7a
Radek262 <rad.dunin@gmailcom>
2016-05-28 11:53:41 +0200

    strive to fix the problem with merge

--- src/AppBundle/Controller/PriceController.php
+++ src/AppBundle/Controller/PriceController.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 
 namespace AppBundle\Controller
@@ -17,4 +18,26 @@ class PriceController extends Controller
         return $this->render('default/uni_price.html.twig');
     }
 }
+=======
+w<?php
+
+namespace AppBundle\Controller {
+
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+    class UniController extends Controller
+    {
+
+        /**
+         * @Route ("/price", name="price")
+
+         */
+        public function aboutAction ()
+        {
+
+            return $this->render('default/uni_price.html.twig');
+        }
+    }
+>>>>>>> Stashed changes
 }
