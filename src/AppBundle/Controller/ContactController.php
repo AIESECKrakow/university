36d6b5f16b4a00024699bcac55d35f5751640ab4
patryk801 <patryk801@aol.pl>
2016-07-17 14:35:25 +0200

    Merged with patryk801Branch

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Group;
+use AppBundle\Entity\Level;
+use AppBundle\Entity\Student;
+use AppBundle\Repositories\GroupsRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Type;
+
+class ContactController extends Controller
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction() {
+        return $this->render('default/contact.html.twig');
+    }
+}
---
+++ src/AppBundle/Controller/PricelistController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Group;
+use AppBundle\Entity\Level;
+use AppBundle\Entity\Student;
+use AppBundle\Repositories\GroupsRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Type;
+
+class PricelistController extends Controller
+{
+    /**
+     * @Route("/pricelist", name="pricelist")
+     */
+    public function priceAction()
+    {
+        return $this->render('default/pricelist.html.twig');
+    }
+}
